176a28dba34689bc4809b431f5ecc2ad5d9a682e
sbmax002 <jwooooy002@gmail.com>
2025-10-11 18:22:01 +0900

    feat: add Image entity - pit 469

---
+++ src/main/java/PitterPatter/loventure/content/domain/image/domain/ImageType.java
@@ -0,0 +1,10 @@
+package PitterPatter.loventure.content.domain.image.domain;
+
+/**
+ * 이미지 타입을 정의하는 Enum
+ */
+public enum ImageType {
+    DIARY,    // 다이어리 이미지
+    PROFILE   // 프로필 이미지 (향후 사용)
+}
+
